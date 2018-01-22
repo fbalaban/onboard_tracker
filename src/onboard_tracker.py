6d346a8410b593f9b3ea18af68c68cfb6f7378f7
fbalaban <fbalampanis@gmail.com>
2018-01-22 22:04:55 +0100

    Update onboard_tracker.py
    Integration integration

--- src/onboard_tracker.py
+++ src/onboard_tracker.py
@@ -18,6 +18,11 @@ global the_home
 global waypoint_iter
 global pr_closest_point
 global mission_started
+global once
+global integration_started
+global integrated_area
+
+integration_started = False
 
 the_home = Waypoint()
 the_home.frame = 0
@@ -32,9 +37,11 @@ the_home.x_lat = 0
 the_home.y_long = 0
 the_home.z_alt = 0
 
-waypoint_iter = 1
+waypoint_iter = 0
 pr_closest_point = tracker.calc.waypoint(0,0)
 mission_started = False
+integrated_area = 0
+once = True
 
 def position_callback(data):
 
@@ -49,6 +56,7 @@ def position_callback(data):
 
     global the_home
     global has_wp_plan
+    global closest_point
 
     if state == "AUTO" and has_wp_plan == True:
         mission_started = True
@@ -59,18 +67,17 @@ def position_callback(data):
 
         closest_point = tracker.calc.get_closest_point_on_segment(previous_wp.latitude, previous_wp.longitude, next_wp.latitude, next_wp.longitude, data.latitude, data.longitude)
 
-        # if waypoint_iter > 0:
-        #     if integration_started == 0:
-        #         rospy.loginfo("========= INTEGRATION STARTED =========")
-        #         vasi_mikri = get_distance(data.latitude, data.longitude, closest_point.latitude, closest_point.longitude)
-        #         pr_closest_point = waypoint(latitude=closest_point.latitude, longitude=closest_point.longitude)
-        #         integration_started = 1
-        #     else:
-        #         vasi_megali  = get_distance(data.latitude, data.longitude, closest_point.latitude, closest_point.longitude)
-        #         integrated_area +=  ((vasi_mikri + vasi_megali) * get_distance(pr_closest_point.latitude, pr_closest_point.longitude,
-        #                                                                      closest_point.latitude, closest_point.longitude)) / 2
-        #         vasi_mikri = vasi_megali
-        #         pr_closest_point = closest_point
+        if waypoint_iter > 0:
+            if integration_started == False:
+                rospy.loginfo("========= INTEGRATION STARTED =========")
+                integration_started = True
+                vasi_mikri = tracker.calc.get_distance(data.latitude, data.longitude, closest_point.latitude, closest_point.longitude)
+                pr_closest_point = tracker.calc.waypoint(latitude=closest_point.latitude, longitude=closest_point.longitude)
+            else:
+                vasi_megali  = tracker.calc.get_distance(data.latitude, data.longitude, closest_point.latitude, closest_point.longitude)
+                integrated_area +=  ((vasi_mikri + vasi_megali) * tracker.calc.get_distance(pr_closest_point.latitude, pr_closest_point.longitude,closest_point.latitude, closest_point.longitude)) / 2
+                vasi_mikri = vasi_megali
+                pr_closest_point = closest_point
 
         bearing = tracker.calc.find_bearing(previous_wp, next_wp)
 
@@ -80,10 +87,10 @@ def position_callback(data):
 
         c_wp = tracker.calc.find_command_waypoint(tracker.const.DISTANCE, bearing, closest_point)
 
-        if tracker.calc.get_distance(data.latitude,data.longitude,next_wp.latitude,next_wp.longitude) < 0.04: # meters
+        if tracker.calc.get_distance(data.latitude,data.longitude,next_wp.latitude,next_wp.longitude) < 0.03: # meters
             waypoint_iter += 1
             rospy.loginfo("[Tracker] Waypoint id: %s", str(waypoint_iter))
-            if waypoint_iter == len(wp_plan):
+            if waypoint_iter == (len(wp_plan) - 1):
                 # introduce angle if needed from previous file
                 rospy.loginfo("[Tracker] For distance of: %s km the integrated area is: %s sq.kilometers ", str(tracker.const.DISTANCE), str(integrated_area))
                 state = "RTL"
@@ -100,20 +107,27 @@ def position_callback(data):
         the_wp.param2 = 10
         the_wp.param3 = 0
         the_wp.param4 = 0
+        #the_wp.x_lat = 37.5200634
+        #the_wp.y_long = -5.8591536
+#the_req.waypoints.insert(1,the_wp)
+#37.5200634
+#-5.8591536
         the_wp.x_lat = c_wp.latitude
         the_wp.y_long = c_wp.longitude
         the_wp.z_alt = tracker.const.ALTITUDE
-
+        #the_wp.z_alt = 41.6
         the_req = WaypointPushRequest()
-        the_req.waypoints.insert(0,the_home)
-        the_req.waypoints.insert(1,the_wp)
+        #the_req.waypoints.insert(0,the_home)
+        #the_req.waypoints.insert(1,the_wp)
+        the_req.waypoints.insert(0,the_wp)
 
         # SENDING THE LIST
         rospy.loginfo("[Tracker] Intermediate waypoint, latitude: %s, longitude: %s ", str(c_wp.latitude), str(c_wp.longitude))
         # TODO maybe add the "the_req.start_index = 1" instead of the full wp list (with home) which will replace only the 1st wp instead of the whole
+
         # list http://docs.ros.org/api/mavros_msgs/html/srv/WaypointPush.html. This would probably mean that the current service wouldn't be needed
         command_service(the_req)
-        current_service(1)
+        #current_service(0)
 
 def update_mission_callback(data):
 
@@ -150,35 +164,36 @@ def state_callback(data):
     global has_wp_plan
     global mission_started
     global waypoint_iter
+    global once
 
-    
     if data.mode == "AUTO" or data.mode == "AUTO.MISSION":
-        waypoint_pull_service()
         if initial_run == True:
+            waypoint_pull_service()
             has_wp_plan = False
             initial_run = False
-        state = "AUTO"
-        rospy.loginfo("[Tracker] Changed to AUTO")
+            state = "AUTO"
+            rospy.loginfo("[Tracker - STATE] AUTO - MISSION")
+        initial_run = False
     if data.mode == "STABILIZED" or data.mode == "STABILIZE":
         state = "STABILIZED"
-        waypoint_iter = 1
+        waypoint_iter = 0
         mission_started = False
         initial_run = True
         has_wp_plan = False
-        clearing_service()
-        rospy.loginfo("[Tracker] Changed to STABILIZED")
+        #clearing_service()
+        rospy.loginfo("[Tracker - STATE] STABILIZED")
     if data.mode == "AUTO.RTL" or data.mode == "RTL":
         state = "RTL"
-        waypoint_iter = 1
+        waypoint_iter = 0
         mission_started = False
         initial_run = True
         has_wp_plan = False
         clearing_service()
-        rospy.loginfo("[Tracker] Changed to RTL - clearing mission")
+        rospy.loginfo("[Tracker - STATE] RTL - clearing mission")
     if data.mode == "MANUAL":
         state = "MANUAL"
-        rospy.loginfo("[Tracker] Changed to MANUAL")
-        rospy.signal_shutdown("[Tracker] Manual mode, gathering ROS bags and shutting down")
+        rospy.loginfo("[Tracker - STATE] MANUAL")
+        rospy.signal_shutdown("[Tracker - INFO] Manual mode, gathering ROS bags and shutting down")
         
 if __name__ == '__main__':
 

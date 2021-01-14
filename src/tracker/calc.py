726871f20d23b070541898967de1e13e8ba35a14
fbalaban <fbalampanis@gmail.com>
2021-01-14 20:45:20 +0100

    Update calc.py

--- src/tracker/calc.py
+++ src/tracker/calc.py
@@ -2,53 +2,61 @@ from collections import namedtuple
 from math import atan2, cos, sin, asin, radians, degrees, sqrt
 
 # R is earth radius
-R = 6371 # in km
+R = 6371  # in km
 waypoint = namedtuple('waypoint', 'latitude longitude')
 
-# find bearing between two waypoints
-# http://www.movable-type.co.uk/scripts/latlong.html (x and y are inverted)
-def find_bearing(a, b):
 
-    x_value = cos(radians(b.latitude)) * ( sin(radians(b.longitude) - radians(a.longitude)) )
-    y_value = cos(radians(a.latitude)) * sin(radians(b.latitude)) - (sin(radians(a.latitude))  * cos(radians(b.latitude)) * cos(radians(b.longitude) - radians(a.longitude)))
-    return degrees(atan2(x_value, y_value))
+def find_bearing(point_a, point_b):
+    """
+    Find bearing between two waypoints
+    (Algorithm:http://www.movable-type.co.uk/scripts/latlong.html (x and y are inverted))
+    """
+    return (atan2((cos(radians(point_b.latitude)) * (sin(radians(point_b.longitude) - radians(point_a.longitude)))),
+                  (cos(radians(point_a.latitude)) * sin(radians(point_b.latitude)) - (sin(radians(point_a.latitude)) *
+                                                                                      cos(radians(point_b.latitude)) *
+                                                                                      cos(radians(point_b.longitude) -
+                                                                                          radians(
+                                                                                              point_a.longitude))))))
 
-# find destination point given a distance and and bearing from a starting point
+
 def find_command_waypoint(dis, bear, starting_waypoint):
-
-    # d is the angular distance
-    d = dis / R
+    """
+    find destination point given a distance and and bearing from a starting point
+    """
+    d = dis / R  # d is the angular distance
     bearing = radians(bear)
-    target_latitude = asin( (sin(radians(starting_waypoint.latitude)) * cos(d) ) + \
-                            (cos(radians(starting_waypoint.latitude)) * sin(d) * cos(bearing)))
-    target_longitude = radians(starting_waypoint.longitude) + \
-                atan2(sin(bearing) * sin(d) * cos(radians(starting_waypoint.latitude)),
-                      cos(d) - sin(radians(starting_waypoint.latitude)) * sin(target_latitude))
-    return waypoint(latitude=round(degrees(target_latitude),7), longitude=round(degrees(target_longitude),7))
+    target_latitude = asin((sin(radians(starting_waypoint.latitude)) * cos(d)) +
+                           (cos(radians(starting_waypoint.latitude)) * sin(d) * cos(bearing)))
 
-#http://www.java2s.com/Code/Java/2D-Graphics-GUI/Returnsclosestpointonsegmenttopoint.htm
+    target_longitude = radians(starting_waypoint.longitude) + atan2(sin(bearing) * sin(d) *
+                                                                    cos(radians(starting_waypoint.latitude)),
+                                                                    cos(d) - sin(radians(starting_waypoint.latitude)) *
+                                                                    sin(target_latitude))
+
+    return waypoint(latitude=round(degrees(target_latitude), 7), longitude=round(degrees(target_longitude), 7))
+
+
 def get_closest_point_on_segment(sx1, sy1, sx2, sy2, px, py):
+    """
+    Getting closest point to segment
+    (Algorithm: http://www.java2s.com/Code/Java/2D-Graphics-GUI/Returnsclosestpointonsegmenttopoint.htm
+    """
 
-    xDelta = sx2 - sx1
-    yDelta = sy2 - sy1
-    u = ((px - sx1) * xDelta + (py - sy1) * yDelta) / (xDelta * xDelta + yDelta * yDelta)
-
-    if u < 0:
-      closestPoint = waypoint(sx1, sy1)
-    elif u > 1:
-      closestPoint = waypoint(sx2, sy2)
+    x_delta = sx2 - sx1
+    y_delta = sy2 - sy1
+    u = ((px - sx1) * x_delta + (py - sy1) * y_delta) / (x_delta * x_delta + y_delta * y_delta)
+    if u == 0:
+        return waypoint(latitude=round((sx1 + u * x_delta), 7), longitude=round((sy1 + u * y_delta), 7))
     else:
-      closestPoint = waypoint(round((sx1 + u * xDelta),7), round((sy1 + u * yDelta),7))
+        return waypoint(latitude=sx1, longitude=sy1) if (u < 0) else waypoint(latitude=sx2, longitude=sy2)
 
-    return closestPoint
 
 def get_distance(lat1, lon1, lat2, lon2):
-
-    f1 = radians(lat1)
-    f2 = radians(lat2)
-    df = radians(lat2-lat1)
-    dl = radians(lon2-lon1)
-
-    a = ( sin(df/2) * sin(df/2) ) + ( cos(f1) * cos(f2) * sin(dl/2) * sin(dl/2) )
-    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    """
+    Getting distance between two points
+    """
+    a = (sin(radians(lat2 - lat1) / 2) * sin(radians(lat2 - lat1) / 2)) + (cos(radians(lat1)) * cos(radians(lat2)) *
+                                                                           sin(radians(lon2 - lon1) / 2) *
+                                                                           sin(radians(lon2 - lon1) / 2))
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
     return R * c
